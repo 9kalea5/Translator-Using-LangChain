55fa817d1bea227d066a63331070dba9eb136e94
kaleab <alebachewkaleab@gmail.com>
2024-11-24 17:28:31 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+groq_api_key=os.environ.get('GROQ_API_KEY')
+
+
+## Prompt Template
+
+prompt=ChatPromptTemplate.from_messages(
+    [
+       (
+        "system",
+        "You are a helpful assistant that translates a given language to English. Translate the user sentence.",
+    ),
+        ("user","Question:{question}")
+    ]
+)
+
+## streamlit framework
+
+
+st.title('Langchain Demo With Groq API')
+input_text=st.text_input("Enter the text you want to translate")
+
+# openAI LLm 
+llm=ChatGroq(groq_api_key=groq_api_key,
+             model_name="Gemma-7b-It")
+output_parser=StrOutputParser()
+chain=prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({'question':input_text}))
